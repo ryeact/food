76f76efe0d10e56fb85a52ef12905cb41d9842c3
Ary Harlestary <aryharlestary@gmail.com>
2020-08-14 00:02:58 +0700

    deploy

---
+++ precache-manifest.e1777526f30c8b8803ba96047b138e02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "946274415875c478a2d7d4da774dd2d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd3a8354c7575c5cc42c",
+    "url": "/static/css/main.9f59252a.chunk.css"
+  },
+  {
+    "revision": "d6556b85338c5b851b96",
+    "url": "/static/js/2.1c941e50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1c941e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd3a8354c7575c5cc42c",
+    "url": "/static/js/main.82c2d2eb.chunk.js"
+  },
+  {
+    "revision": "28648d4b86d330626cee",
+    "url": "/static/js/runtime-main.fafa68b7.js"
+  },
+  {
+    "revision": "87b5f04a82a6dc765fcfebba13b6d9c8",
+    "url": "/static/media/aneka-makanan.87b5f04a.png"
+  }
+]);
